0ca05c739a6f41c078eef5dee592ff5c67156287
goryo <goryo.webdev@gmail.com>
2014-07-14 13:59:42 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/**
+* Created with pelikula.
+* User: stoned-code-warrior
+* Date: 2014-06-24
+* Time: 10:23 PM
+* To change this template use Tools | Templates.
+*/
+define(function() {
+return {};
+});
