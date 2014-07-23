1b31e931ab9961083f0be46eb365c570034b652e
goryo <goryo.webdev@gmail.com>
2014-07-23 15:53:06 +0800

    update

--- malunggaypy.py
+++ malunggaypy.py
@@ -13,12 +13,21 @@ from google.appengine.api import users
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
+#models
+class MainEntries(db.Model):
+  titulo = db.StringProperty(required=True)
+  kategorya = db.StringProperty(required=True) #pelikula, teleserye, palabas
+  letrato_link = db.StringProperty(required=True)
+#  uri = db.StringProperty(required=True) 
+  date_created = db.DateTimeProperty(auto_now_add=True)
+  date_updated = db.DateProperty()
 
+# controllers
 class MainPage(webapp2.RequestHandler):
   def get(self):       
-
+    
     template_values = {
-     
+      
     }
 
     template = jinja_environment.get_template('index.html')
