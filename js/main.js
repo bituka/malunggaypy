5b19e414dbd5d42f41242b31ac7d5af9850edb68
goryo <goryo.webdev@gmail.com>
2014-08-11 15:32:05 +0800

    jquery display next row fix

--- js/main.js
+++ js/main.js
@@ -5,6 +5,15 @@
 * Time: 10:23 PM
 * To change this template use Tools | Templates.
 */
+$(function () {
+//	alert('test');
+	$('div.pelikula-entries:nth-child(4n)').after('</div><div class="row">');
+});
+$(document).ready(function() {
+//	alert('test');
+});
+/*
 define(function() {
 return {};
 });
+*/
